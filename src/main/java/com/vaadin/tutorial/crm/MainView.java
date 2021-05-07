4ffb3ec6b5a2a2b05237a8e4d2a3343cfa03cb0e
Zone-Infinity <sohamdokhale1602@gmail.com>
2021-05-07 14:59:23 +0530

    first commit

---
+++ src/main/java/com/vaadin/tutorial/crm/MainView.java
@@ -0,0 +1,32 @@
+package com.vaadin.tutorial.crm;
+
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.PWA;
+
+/**
+ * A sample Vaadin view class.
+ * <p>
+ * To implement a Vaadin view just extend any Vaadin component and
+ * use @Route annotation to announce it in a URL as a Spring managed
+ * bean.
+ * Use the @PWA annotation make the application installable on phones,
+ * tablets and some desktop browsers.
+ * <p>
+ * A new instance of this class is created for every new user and every
+ * browser tab/window.
+ */
+@Route
+@PWA(name = "Vaadin Application",
+        shortName = "Vaadin App",
+        description = "This is an example Vaadin application.",
+        enableInstallPrompt = false)
+@CssImport("./styles/shared-styles.css")
+@CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+
+    }
+}
